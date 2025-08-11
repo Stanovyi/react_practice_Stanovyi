57ad4e9e35b6a30a1416b3fd92f9805c4b0a62d1
Ihor Stanovyi <ihor.stanovyi@gmail.com>
2025-08-11 09:50:48 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@
 import React from 'react';
 import './App.scss';
 
-// import usersFromServer from './api/users';
-// import categoriesFromServer from './api/categories';
-// import productsFromServer from './api/products';
+import usersFromServer from './api/users';
+import categoriesFromServer from './api/categories';
+import productsFromServer from './api/products';
 
-// const products = productsFromServer.map((product) => {
-//   const category = null; // find by product.categoryId
-//   const user = null; // find by category.ownerId
+const products = productsFromServer.map((product) => {
+  const category = null; // find by product.categoryId
+  const user = null; // find by category.ownerId
 
-//   return null;
-// });
+  return null;
+});
 
 export const App = () => (
   <div className="section">
